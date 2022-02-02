741490e80e157b25a1bd1e514005ba4ef618dab3
HemanthCU <hech9374@colorado.edu>
2022-02-02 07:19:20 -0700

    Updated base server code

--- buyer/buyer-server.py
+++ buyer/buyer-server.py
@@ -3,7 +3,20 @@
 from socket import * 
 import thread
 
+serverPort = 8888
 
+def threadrunner(clientsock, addr):
+	while 1:
+		data = clientsock.recv(1024)
+		if not data: break
+		# Code to be written here
 
 if __name__ == '__main__':
-    
+    tcpsocket = socket(AF_INET, SOCK_STREAM)
+    tcpsocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    tcpsocket.bind(('', serverPort))
+    tcpsocket.listen(5)
+
+while 1:
+	(clientsock, addr) = tcpsocket.accept()
+	thread.start_new_thread(threadrunner, (clientsock, addr))
---
+++ seller/seller-server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from socket import * 
+import thread
+
+serverPort = 8887
+
+def threadrunner(clientsock, addr):
+	while 1:
+		data = clientsock.recv(1024)
+		if not data: break
+		# Code to be written here
+
+if __name__ == '__main__':
+    tcpsocket = socket(AF_INET, SOCK_STREAM)
+    tcpsocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    tcpsocket.bind(('', serverPort))
+    tcpsocket.listen(5)
+
+while 1:
+	(clientsock, addr) = tcpsocket.accept()
+	thread.start_new_thread(threadrunner, (clientsock, addr))
