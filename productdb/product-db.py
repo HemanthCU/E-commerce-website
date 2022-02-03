293f9cbbcbb11394600a484f9025c1077b577a3f
HemanthCU <hech9374@colorado.edu>
2022-02-03 05:19:01 +0000

    Updated productdb

--- productdb/product-db.py
+++ productdb/product-db.py
@@ -5,11 +5,36 @@ import thread
 
 serverPort = 8886
 
+productdb = []
+
 def threadrunner(clientsock, addr):
 	while 1:
 		data = clientsock.recv(1024)
-		if not data: break
-		# Code to be written here
+		if not data:
+            break
+        #Input format: CMD Name Category ID Condition Price Quantity Keyword1...
+		cmd, data = data.split(' ', 1)
+        #List of commands: GET(based on ID) ADD UPDATE REMOVE
+        if cmd in ['GET']:
+            iid, data = data.split(' ', 1)
+            iquant = int(data)
+        elif cmd in ['ADD']:
+            iname, data = data.split(' ', 1)
+            icat, data = data.split(' ', 1)
+            iid, data = data.split(' ', 1)
+            icond, data = data.split(' ', 1)
+            iprice, data = data.split(' ', 1)
+            iquant, data = data.split(' ', 1)
+            iquant = int(iquant)
+            klist = data.split()
+        elif cmd in ['UPDATE']:
+            iid, data = data.split(' ', 1)
+            iprice = data
+        elif cmd in ['REMOVE']:
+            iid, data = data.split(' ', 1)
+            iquant = int(data)
+        #elif cmd in ['']:
+        #TODO: Search based on keywords
 
 if __name__ == '__main__':
     tcpsocket = socket(AF_INET, SOCK_STREAM)
