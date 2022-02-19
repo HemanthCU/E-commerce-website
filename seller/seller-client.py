ed37d364a09714b538341813715daf475602a05b
HemanthCU <hech9374@@colorado.edu>
2022-02-19 21:57:24 +0000

    Updated seller client

--- seller/seller-client.py
+++ seller/seller-client.py
@@ -3,6 +3,14 @@ import socket
 import threading
 import sys
 
+from __future__ import print_function
+import requests
+import json
+import time
+import base64
+import jsonpickle
+import random
+
 # Create an account: sets up username and password 0000
 # Login: provide username and password 0001
 # Logout 0010
@@ -15,14 +23,7 @@ import sys
 
 #CMD ARG1 ARG2 ARG3 ARG4 ARG5
 
-def threadrunner(host_ip, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print ("Socket is successfully created")
-    except socket.error as err:
-        print ("Socket creation is failed with error %s" %(err))
-    s.connect((host_ip, port))
-    print("Seller client is ready") 
+def threadrunner(addr):
     while 1:
         print("MENU")
         print("0100 - Put an item for sale")
@@ -34,37 +35,65 @@ def threadrunner(host_ip, port):
         print("cmd "+val)
         if val== "exit":
             print("exiting")
-            s.send('1111'.encode())
+            #s.send('1111'.encode())
             break
         if val=='0100':
+            headers = {'content-type': 'application/json'}
+            url = addr + "/api/put"
             #Put an item for sale
             #seller id, quantity
-            keywords = input("Enter your Item name, category, condition, sale price, quantity and key words with space: ")
-            s.send((val + " "+keywords).encode())
+            inputstr = input("Enter your Item name, category, condition, sale price, quantity and key words with space: ")
+            values = {
+                'inputstr' : inputstr
+            }
+            response = requests.post(url, data=jsonpickle.encode(values), headers=headers)
+            
+            #s.send((val + " "+keywords).encode())
             print("Checking whether success")
-            print (s.recv(1024).decode())#recv has to be a blocking call
+            #print (s.recv(1024).decode())#recv has to be a blocking call
         if val=='0101':
+            headers = {'content-type': 'application/json'}
+            url = addr + "/api/change"
             #Change the sale price of an item
             ItemId_price = input("Enter item ID and new sale price: ")
-            s.send((val+' '+ItemId_price).encode())
+            values = {
+                'inputstr' : ItemId_price
+            }
+            response = requests.post(url, data=jsonpickle.encode(values), headers=headers)
+            #s.send((val+' '+ItemId_price).encode())
             print("Checking whether success !!")
-            print (s.recv(1024).decode())#recv has to be a blocking call
+            #print (s.recv(1024).decode())#recv has to be a blocking call
         if val=="0110":
+            headers = {'content-type': 'application/json'}
+            url = addr + "/api/remove"
             #Remove an item from sale
             ItemId_quantity = input("Enter item ID and quantity: ")
-            s.send((val+' '+ItemId_quantity).encode())
-            print("Checking whether success !!")
-            data = s.recv(1024).decode()
-            if not data.split(' ', 1) == 'GETFAILURE':
-                print (data)#recv has to be a blocking call
+            values = {
+                'inputstr' : ItemId_quantity
+            }
+            response = requests.post(url, data=jsonpickle.encode(values), headers=headers)
+            #s.send((val+' '+ItemId_quantity).encode())
+            #print("Checking whether success !!")
+            #data = s.recv(1024).decode()
+            #if not data.split(' ', 1) == 'GETFAILURE':
+            #    print (data)#recv has to be a blocking call
         if val=="0111":
+            headers = {'content-type': 'application/json'}
+            url = addr + "/api/display"
             #Display items currently on sale put up by this seller
             seller_id = input("Enter seller id: ")
-            s.send((val+" "+seller_id).encode())
+            values = {
+                'inputstr' : seller_id
+            }
+            response = requests.post(url, data=jsonpickle.encode(values), headers=headers)
+            #s.send((val+" "+seller_id).encode())
             print("Checking whether success !!")
-            print (s.recv(1024).decode())#recv has to be a blocking call
+            #print (s.recv(1024).decode())#recv has to be a blocking call
 
 port = 8807
+host = '127.0.0.1'
+
+addr = f"http://{host}:8807"
 try:
     host_ip = socket.gethostbyname('127.0.0.1')
 except socket.gaierror:
@@ -74,7 +103,7 @@ while 1:
     val = input("Do you want to use buyer-client interface: ")
     if val=='yes':
     #threading.Thread(target = threadrunner, args = (host_ip, port,)).start()
-        threadrunner(host_ip,port)
+        threadrunner(addr)
     elif val=='exit':
         break
 
