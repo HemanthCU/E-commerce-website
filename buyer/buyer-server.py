1df8230c5ae64943ced03e203e8fab3973b3e857
HemanthCU <hech9374@@colorado.edu>
2022-02-21 05:50:07 +0000

    Deleted old code
    Took a zip file of all the files before deleting

--- buyer/buyer-server.py
+++ buyer/buyer-server.py
@@ -125,6 +125,7 @@ def displayCart():
     response_pickled = jsonpickle.encode(response)
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
+# Clear the shopping cart CMD 0110
 @app.route('/api/clearCart', methods=['POST'])
 def clearCart():
     r = request
@@ -139,105 +140,5 @@ def clearCart():
     response_pickled = jsonpickle.encode(response)
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
-def threadrunner(clientsock, addr):
-    global stub
-    print(clientsock)
-    print(addr)
-    #product DB connection
-    productDB_port = 8886
-    try:
-        productDB_ip = ''
-    except gaierror:
-        print ("there was an error resolving the host")
-        sys.exit()
-    try:
-        productDB_socket = socket(AF_INET, SOCK_STREAM)
-        print ("Socket is successfully created")
-    except error as err:
-        print ("Socket creation is failed with error %s" %(err))
-    productDB_socket.connect((productDB_ip, productDB_port))
-
-    shoppingCart = {}
-    while 1:
-        print('Waiting for user command')
-        data = clientsock.recv(1024).decode()
-        print(data)
-        cmd,data = data.split(' ',1)
-        print(cmd)
-        if cmd=='1111':
-            break
-        if cmd == '0011':
-            #search items for sale
-            index = 1
-            keywords = data.split(' ')
-            catagory = keywords[0]
-            finalItems = ''
-            while(index<len(keywords)):
-                #productDB_socket.send(("GETIIDS "+keywords[index]).encode())
-                #itemIDList = productDB_socket.recv(1024).decode()
-                responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = "GETIIDS "+keywords[index]))
-                itemIDList = responseFromDB
-                print(itemIDList)
-                itemList = itemIDList.split(' ')
-                for itemID in itemList:
-                    productDB_socket.send(("GET "+itemID).encode())
-                    itemDetails = productDB_socket.recv(1024).decode()
-                    if not itemDetails.split(' ')[0] in ['GETFAILURE']:
-                        print(itemDetails)
-                        itemDetailstTuple = itemDetails.split(' ')
-                        if itemDetailstTuple[2] in [catagory] and int(itemDetailstTuple[5])>0:
-                            finalItems += itemDetailstTuple[0]+' '+itemDetailstTuple[1]+' '+itemDetailstTuple[2]+' '+itemDetailstTuple[3]+' '+itemDetailstTuple[4]+' '+itemDetailstTuple[5]+'\n'
-                index += 1
-            clientsock.send(finalItems.encode())
-        if cmd=='0100':
-			#add item to shopping cart
-            itemDetails = data.split(' ')
-            #productDB_socket.send(("GET "+itemDetails[0]).encode())
-            #itemDBDetails = productDB_socket.recv(1024).decode()
-            responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = "GET "+itemDetails[0]))
-            itemDBDetails = responseFromDB
-            if itemDBDetails.split(' ')[0] in ['GETFAILURE']:
-                clientsock.send("Item is not available currently".encode())
-            else:    
-            if itemDetails[0] in shoppingCart.keys():
-                shoppingCart[itemDetails[0]] = str(int(shoppingCart[itemDetails[0]]) + int(itemDetails[1]))
-            else:
-                shoppingCart[itemDetails[0]] = itemDetails[1]
-            clientsock.send("Successfully done ".encode())
-        if cmd=='0111':
-			#Display shopping cart
-            currentCart = ''
-            for key in shoppingCart.keys():
-                if int(shoppingCart[key])>0:
-                    currentCart = currentCart +" item id : " +str(key)+", quantity: "+str(shoppingCart[key])+"\n"
-            if currentCart == '':
-                currentCart = 'CART EMPTY'
-            clientsock.send(currentCart.encode())
-        if cmd=='0101':
-           # remove an item from cart
-            itemDetails = data.split(' ')
-            if itemDetails[0] in shoppingCart.keys():
-                shoppingCart[itemDetails[0]] = str(int(shoppingCart[itemDetails[0]]) - int(itemDetails[1]))
-                if int(shoppingCart[itemDetails[0]])<=0:
-                    shoppingCart.pop(itemDetails[0])
-                clientsock.send("Successfully done ".encode())
-            else:
-                clientsock.send("Item doesn't exist ".encode())
-        if cmd=='0110':
-			#clear the cart
-            shoppingCart.clear()
-            clientsock.send("Successfully done ".encode())
-        print("shopping cart")
-        print(shoppingCart)
-
 app.run(host="0.0.0.0", port=8808)
-#if __name__ == '__main__':
-#    tcpsocket = socket(AF_INET, SOCK_STREAM)
-#    tcpsocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-#    tcpsocket.bind(('', SERVERPORT))
-#    tcpsocket.listen(5)
-
-#while 1:
-#	(clientsock, addr) = tcpsocket.accept()
-#	threading.Thread(target = threadrunner, args = (clientsock, addr,)).start()
     
--- seller/seller-client.py
+++ seller/seller-client.py
@@ -92,12 +92,10 @@ def threadrunner(addr):
             }
             response = requests.post(url, data=jsonpickle.encode(values), headers=headers)
             
-            #s.send((val + " "+keywords).encode())
             print("Checking whether success")
             #dict_data = json.loads(response.get_json())
             print(json.loads(response.text)['result'])
             print(response)
-            #print (s.recv(1024).decode())#recv has to be a blocking call
         if val=='0101':
             headers = {'content-type': 'application/json'}
             url = addr + "/api/changeSalesPrice"
@@ -107,12 +105,9 @@ def threadrunner(addr):
                 'inputstr' : ItemId_price
             }
             response = requests.post(url, data=jsonpickle.encode(values), headers=headers)
-            #s.send((val+' '+ItemId_price).encode())
             print("Checking whether success !!")
             print(json.loads(response.text)['result'])
             print(response)
-            
-            #print (s.recv(1024).decode())#recv has to be a blocking call
         if val=="0110":
             headers = {'content-type': 'application/json'}
             url = addr + "/api/removeItem"
@@ -134,7 +129,6 @@ def threadrunner(addr):
                 'inputstr' : seller_id
             }
             response = requests.post(url, data=jsonpickle.encode(values), headers=headers)
-            #s.send((val+" "+seller_id).encode())
             print("Checking whether success !!")
             print(json.loads(response.text)['result'])
             print(response)
