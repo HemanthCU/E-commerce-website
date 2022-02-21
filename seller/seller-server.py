0f7ea3a820dac27c20603818dcbbaf0ce3b57fc3
HemanthCU <hech9374@@colorado.edu>
2022-02-21 06:52:57 +0000

    Removed old code + tabbing

--- seller/seller-server.py
+++ seller/seller-server.py
@@ -66,18 +66,18 @@ def logIn():
     responseFromCustomerDB = stub1.sendCustomerDB(customer_pb2.inputMsg(input = inputCmd))
     outputStr = responseFromCustomerDB.output
     if outputStr.split(' ',1) == 'LoggedIn':
-      logedInBuyerList[outputStr] = 1
-      response = {
+        logedInBuyerList[outputStr] = 1
+        response = {
         'result': 'Log in successful'
-      }
-      response_pickled = jsonpickle.encode(response)
-      return Response(response=response_pickled, status=200, mimetype="application/json")
+        }
+        response_pickled = jsonpickle.encode(response)
+        return Response(response=response_pickled, status=200, mimetype="application/json")
     else:
-      response = {
+        response = {
         'result': 'Log in unsuccessful'
-      }
-      response_pickled = jsonpickle.encode(response)
-      return Response(response=response_pickled, status=200, mimetype="application/json")
+        }
+        response_pickled = jsonpickle.encode(response)
+        return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
 @app.route('/api/logOut', methods=['POST'])
@@ -167,9 +167,9 @@ def change():
     elif logedInBuyerList[username] != 1:
         outputStr = 'please log in first'
     else:
-      inputstr = 'UPDATE ' + inputCmd
-      responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = inputstr))
-      outputStr = responseFromDB.output
+        inputstr = 'UPDATE ' + inputCmd
+        responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = inputstr))
+        outputStr = responseFromDB.output
     
     response = {
         'result': outputStr
@@ -197,11 +197,11 @@ def display():
         itemList = outputStr1.split(' ')
         resultItemList = ''
         for iid in itemList:
-         responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = 'GET '+iid))
-         res = responseFromDB.output
-         print(res)
-         if not res.split(' ')[0] in ['GETFAILURE']:
-            resultItemList += res +'\n'
+            responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = 'GET '+iid))
+            res = responseFromDB.output
+            print(res)
+            if not res.split(' ')[0] in ['GETFAILURE']:
+                resultItemList += res +'\n'
         outputStr = resultItemList
     response = {
         'result': outputStr
@@ -224,9 +224,9 @@ def remove():
     elif logedInBuyerList[username] != 1:
         outputStr = 'please log in first'
     else:
-      inputstr = 'REMOVE ' + imputCmd
-      responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = inputstr))
-      outputStr = responseFromDB.output
+        inputstr = 'REMOVE ' + imputCmd
+        responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = inputstr))
+        outputStr = responseFromDB.output
     
     response = {
         'result': responseFromDB.output
@@ -247,81 +247,6 @@ def close():
     # encode response using jsonpickle
     response_pickled = jsonpickle.encode(response)
     return Response(response=response_pickled, status=200, mimetype="application/json")
-    
-    
-# def threadrunner(clientsock, addr):
-#     global unique_item_id
-#     global unique_seller_id
-#     global sellerDB
-#     print(clientsock)
-#     print(addr)
-    
-#     product DB connection
-#     productDB_ip =  ''
-#     productDB_port = 8886
-#     try:
-#         productDB_socket = socket(AF_INET, SOCK_STREAM)
-#         print ("Socket is successfully created")
-#     except error as err:
-#         print ("Socket creation is failed with error %s" %(err))
-#     productDB_socket.connect((productDB_ip, productDB_port))
-
-#     while 1:
-#         print('Waiting for user command')
-#         data = clientsock.recv(1024).decode()
-#         print(data)
-#         cmd,arg = data.split(' ',1)
-#         print(cmd)
-#         if cmd=='1111':
-#             break
-#         elif cmd == '0100':
-#             Put an item for sale:
-
-#             preparing seller DB
-#             itemId = str(unique_seller_id)+str(unique_item_id)
-#             if str(unique_seller_id) in sellerDB.keys():
-#                 list1 = sellerDB[str(unique_seller_id)]
-#                 list1.append(itemId)
-#                 sellerDB[str(unique_seller_id)] = list1
-#             else:
-#                 sellerDB[str(unique_seller_id)] = [itemId]
-#             unique_seller_id = unique_seller_id + 1
-#             unique_item_id = unique_item_id + 1
-
-#            preparing productDB 
-#             productDB_socket.send(('ADD '+itemId+' '+arg).encode())
-#             clientsock.send(productDB_socket.recv(1024))
-#         elif cmd=='0101':
-# 			Change the sale price of an item
-#             productDB_socket.send(('UPDATE '+arg).encode())
-#             clientsock.send(productDB_socket.recv(1024))
-#         elif cmd=='0110':
-# 			Remove an item from sale
-#             productDB_socket.send(('REMOVE '+arg).encode())
-#             clientsock.send(productDB_socket.recv(1024))
-#         elif cmd=='0111':
-#            Display items currently on sale put up by this seller
-#             itemList = sellerDB[arg]
-#             resultItemList = ''
-#             for iid in itemList:
-#                 productDB_socket.send(('GET '+iid).encode())
-#                 res = productDB_socket.recv(1024).decode()
-#                 if not res.split(' ')[0] in ['GETFAILURE']:
-#                     resultItemList += res +'\n'
-#             clientsock.send(resultItemList.encode())
-#         print(sellerDB)
-
-#if __name__ == '__main__':
-#    tcpsocket = socket(AF_INET, SOCK_STREAM)
-#    tcpsocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-#    tcpsocket.bind(('', SERVERPORT))
-#    tcpsocket.listen(5)
-
-
-#while 1:
-#	(clientsock, addr) = tcpsocket.accept()
-#	threading.Thread(target = threadrunner, args = (clientsock, addr,)).start()
-
     
 # start flask app
 app.run(host="0.0.0.0", port=8807)
