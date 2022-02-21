769d3272aa34a504aa1acab7da1deeb3a79de84d
HemanthCU <hech9374@@colorado.edu>
2022-02-21 01:46:54 +0000

    Updated buyer server

--- buyer/buyer-server.py
+++ buyer/buyer-server.py
@@ -10,8 +10,8 @@ sys.path.append('../')
 import backend_pb2
 import backend_pb2_grpc
 
-str = '127.0.0.1'+':50052'
-channel = grpc.insecure_channel(str)
+host1 = '127.0.0.1'+':50052'
+channel = grpc.insecure_channel(host1)
 stub = backend_pb2_grpc.backendApiStub(channel)
 SERVERHOST = ''
 SERVERPORT = 8808
@@ -28,6 +28,117 @@ SERVERPORT = 8808
 # Get seller rating: provide buyer id CMD 1010
 # Get buyer history CMD 1011
 # Exit 1111
+
+shoppingCart = {}
+
+# Search items for sale: provide an item category and up to five keywords CMD 0011
+@app.route('/api/searchItems', methods=['POST'])
+def searchItems():
+    r = request
+    global stub
+    json_data = r.get_json()
+    index = 1
+    keywords = json_data['inputstr'].split(' ')
+    category = keywords[0]
+    finalItems = ''
+    while(index<len(keywords)):
+        responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = "GETIIDS "+keywords[index]))
+        itemIDList = responseFromDB.output
+        print(itemIDList)
+        itemList = itemIDList.split(' ')
+        for itemID in itemList:
+            itemDetails = stub.sendProductDB(backend_pb2.inputMsg(input = "GET "+itemID))
+            if not itemDetails.split(' ')[0] in ['GETFAILURE']:
+                print(itemDetails)
+                itemDetailstTuple = itemDetails.split(' ')
+                if itemDetailstTuple[2] in [category] and int(itemDetailstTuple[5])>0:
+                    finalItems += itemDetailstTuple[0]+' '+itemDetailstTuple[1]+' '+itemDetailstTuple[2]+' '+itemDetailstTuple[3]+' '+itemDetailstTuple[4]+' '+itemDetailstTuple[5]+'\n'
+        index += 1
+    response = {
+        'result': responseFromDB.output
+    }
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+# Add item to the shopping cart: provide item id and quantity CMD 0100
+@app.route('/api/addItem', methods=['POST'])
+def addItem():
+    r = request
+    global stub
+    global shoppingCart
+    json_data = r.get_json()
+    itemDetails = json_data['inputstr']
+    responseFromDB = stub.sendProductDB(backend_pb2.inputMsg(input = "GET "+itemDetails[0]))
+    itemDBDetails = responseFromDB.output
+    if itemDBDetails.split(' ')[0] in ['GETFAILURE']:
+        respstr = "Item is not available currently"
+    else:
+        respstr = "Item added successfully"
+        if itemDetails[0] in shoppingCart.keys():
+            shoppingCart[itemDetails[0]] = str(int(shoppingCart[itemDetails[0]]) + int(itemDetails[1]))
+        else:
+            shoppingCart[itemDetails[0]] = itemDetails[1]
+    response = {
+        'result': respstr
+    }
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+# Remove item from the shopping cart: provide item id and quantity CMD 0101
+@app.route('/api/removeItem', methods=['POST'])
+def removeItem():
+    r = request
+    global stub
+    global shoppingCart
+    json_data = r.get_json()
+    itemDetails = json_data['inputstr']
+    if itemDetails[0] in shoppingCart.keys():
+        shoppingCart[itemDetails[0]] = str(int(shoppingCart[itemDetails[0]]) - int(itemDetails[1]))
+        if int(shoppingCart[itemDetails[0]])<=0:
+            shoppingCart.pop(itemDetails[0])
+        respstr = "Successfully done "
+    else:
+        respstr = "Item doesn't exist "
+    response = {
+        'result': respstr
+    }
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+# Display shopping cart CMD 0111
+@app.route('/api/displayCart', methods=['POST'])
+def displayCart():
+    r = request
+    global stub
+    global shoppingCart
+    json_data = r.get_json()
+    dummy = json_data['inputstr']
+    currentCart = ''
+    for key in shoppingCart.keys():
+        if int(shoppingCart[key])>0:
+            currentCart = currentCart +" item id : " +str(key)+", quantity: "+str(shoppingCart[key])+"\n"
+    if currentCart == '':
+        currentCart = 'CART EMPTY'
+    response = {
+        'result': currentCart
+    }
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+@app.route('/api/clearCart', methods=['POST'])
+def clearCart():
+    r = request
+    global stub
+    global shoppingCart
+    json_data = r.get_json()
+    dummy = json_data['inputstr']
+    shoppingCart.clear()
+    response = {
+        'result': "Successfully done "
+    }
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
 def threadrunner(clientsock, addr):
     global stub
     print(clientsock)
@@ -88,11 +199,11 @@ def threadrunner(clientsock, addr):
             if itemDBDetails.split(' ')[0] in ['GETFAILURE']:
                 clientsock.send("Item is not available currently".encode())
             else:    
-             if itemDetails[0] in shoppingCart.keys():
+            if itemDetails[0] in shoppingCart.keys():
                 shoppingCart[itemDetails[0]] = str(int(shoppingCart[itemDetails[0]]) + int(itemDetails[1]))
-             else:
+            else:
                 shoppingCart[itemDetails[0]] = itemDetails[1]
-             clientsock.send("Successfully done ".encode())
+            clientsock.send("Successfully done ".encode())
         if cmd=='0111':
 			#Display shopping cart
             currentCart = ''
@@ -118,15 +229,15 @@ def threadrunner(clientsock, addr):
             clientsock.send("Successfully done ".encode())
         print("shopping cart")
         print(shoppingCart)
-          
-          
-if __name__ == '__main__':
-    tcpsocket = socket(AF_INET, SOCK_STREAM)
-    tcpsocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    tcpsocket.bind(('', SERVERPORT))
-    tcpsocket.listen(5)
 
-while 1:
-	(clientsock, addr) = tcpsocket.accept()
-	threading.Thread(target = threadrunner, args = (clientsock, addr,)).start()
+app.run(host="0.0.0.0", port=8808)
+#if __name__ == '__main__':
+#    tcpsocket = socket(AF_INET, SOCK_STREAM)
+#    tcpsocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+#    tcpsocket.bind(('', SERVERPORT))
+#    tcpsocket.listen(5)
+
+#while 1:
+#	(clientsock, addr) = tcpsocket.accept()
+#	threading.Thread(target = threadrunner, args = (clientsock, addr,)).start()
     
