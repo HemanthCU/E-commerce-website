b8472be1ccb7527f0135bae59330ce81c38b3666
Hemanth Chenna <89418715+HemanthCU@users.noreply.github.com>
2022-04-08 21:34:42 -0600

    Update run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -1,9 +1,10 @@
-python3 productdb/product-db.py
+python3 productdb/raft-psync.py 127.0.0.1:9806  127.0.0.1:9806  127.0.0.1:9806  127.0.0.1:9806 127.0.0.1:9806
+python3 productdb/product-db.py 127.0.0.1:9806  127.0.0.1:9806  127.0.0.1:9806  127.0.0.1:9806 127.0.0.1:9806
 python3 customerDB/customerDB.py
 
-python3 seller/seller-server.py
-python3 seller/seller-client.py
+python3 seller/seller-server.py 127.0.0.1 127.0.0.1
+python3 seller/seller-client.py 127.0.0.1
 
-python3 buyer/buyer-server.py
-python3 buyer/buyer-client.py
+python3 buyer/buyer-server.py 127.0.0.1 127.0.0.1
+python3 buyer/buyer-client.py 127.0.0.1
 
